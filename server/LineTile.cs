9d4307e38b0daf0ba3ecbe0172dfcc5ae4e1598c
Williaminer <73969956+Williaminer@users.noreply.github.com>
2021-11-05 13:52:27 +0000

    Add files via upload

---
+++ server/LineTile.cs
@@ -0,0 +1,16 @@
+﻿namespace ClientExec
+{
+    class LineTile
+    {
+        public string Player { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public LineTile(string colour, int x, int y)
+        {
+            Player = colour;
+            X = x;
+            Y = y;
+        }
+    }
+}
