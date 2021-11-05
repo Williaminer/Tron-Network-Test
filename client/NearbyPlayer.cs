8b84ac49565884509ad61f24085ecc4f71d92618
Williaminer <73969956+Williaminer@users.noreply.github.com>
2021-11-05 13:54:35 +0000

    Add files via upload

---
+++ client/NearbyPlayer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace ClientExec
+{
+    class NearbyPlayer
+    {
+        public string Username { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int LastReceived { get; set; }
+
+        public NearbyPlayer(string username, int x, int y)
+        {
+            LastReceived = 10;
+            Username = username;
+            X = x;
+            Y = y;
+        }
+    }
+}
